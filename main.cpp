7c3166dafafe5dddd018bcf224e49328407f8289
Abhishek Tiwari <51175997+abhishek8953@users.noreply.github.com>
2025-04-06 13:05:39 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ const char* firebaseHost = "";
 const char* firebaseAuth = "";
 
 // Relay pins (changed pin 2 to 25)
-const int relayPins[8] = {25, 4, 5, 18, 19, 21, 22, 23};
+const int relayPins[11] = {5, 18, 19, 22, 23, 13, 12, 14, 26, 27, 32};
 
 // Local state of relay values
-bool relayStates[8] = {false, false, false, false, false, false, false, false};
+bool relayStates[11] = {false, false, false, false, false, false, false, false, false, false, false};
 
 // WiFi connection status pin
 const int wifiStatusPin = 2;
 
 // Function to initialize relays
 void initRelays() {
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 11; i++) {
     pinMode(relayPins[i], OUTPUT);
     digitalWrite(relayPins[i], HIGH); // Turn off all relays initially
   }
@@ -29,7 +29,7 @@ void initRelays() {
 
 // Function to update relay states
 void updateRelays(const JsonObject& json) {
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 11; i++) {
     String relayKey = "relay" + String(i + 1);
     if (json.containsKey(relayKey)) {
       bool state = json[relayKey];
@@ -54,34 +54,18 @@ void handleFirebaseData(const JsonObject& json) {
     } else if (path.startsWith("/relay")) {
       // Single relay update
       int relayIndex = path.substring(6).toInt() - 1;
-      if (relayIndex >= 0 && relayIndex < 8 && data.is<bool>()) {
+      if (relayIndex >= 0 && relayIndex < 11 && data.is<bool>()) {
         bool state = data.as<bool>();
         digitalWrite(relayPins[relayIndex], state ? LOW : HIGH); // HIGH to turn off, LOW to turn on
         relayStates[relayIndex] = state;
-        Serial.printf("Relay %d set to %s\n", relayIndex + 1, state ? "OFF" : "ON");
+        Serial.printf("Relay %d set to %s\n", relayIndex + 1, state ? "ON" : "OFF");
       }
     }
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-
-  pinMode(wifiStatusPin, OUTPUT);
-  digitalWrite(wifiStatusPin, LOW); // Initially set to LOW
-
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println(" connected!");
-  digitalWrite(wifiStatusPin, HIGH); // Set to HIGH when connected
-
-  initRelays();
-
-  // Start HTTP request to Firebase with keep-alive header
+// Function to connect to Firebase
+void connectToFirebase() {
   HTTPClient http;
   String url = String(firebaseHost) + "/relays.json?auth=" + firebaseAuth;
   http.begin(url);
@@ -112,6 +96,25 @@ void setup() {
   http.end();
 }
 
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  pinMode(wifiStatusPin, OUTPUT);
+  digitalWrite(wifiStatusPin, LOW); // Initially set to LOW
+
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println(" connected!");
+  digitalWrite(wifiStatusPin, HIGH); // Set to HIGH when connected
+
+  initRelays();
+  connectToFirebase();
+}
+
 void loop() {
   // Only handle WiFi reconnection
   if (WiFi.status() != WL_CONNECTED) {
@@ -121,5 +124,9 @@ void loop() {
     delay(1000);
   } else {
     digitalWrite(wifiStatusPin, HIGH); // Set to HIGH when connected
+    Serial.print("hello");
+
+    // Reconnect to Firebase if necessary
+    connectToFirebase();
   }
 }
